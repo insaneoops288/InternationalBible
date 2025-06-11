f46455348813cee8e36c90934cb2aa258505e8a3
WhatShouldWedo <insaneoops288@gmail.com>
2025-06-12 05:58:54 +0900

    1

--- Source/InternationalBible/InternationalBibleGameMode.cpp
+++ Source/InternationalBible/InternationalBibleGameMode.cpp
@@ -24,18 +24,21 @@ void AInternationalBibleGameMode::BeginPlay()
 	Super::BeginPlay();
 
 	UBibleGameInstance* GameInstance = Cast<UBibleGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+
+	// UDataTable* SkeletalMeshAsset3 = LoadObject<UDataTable>(NULL, Tables[0], NULL, LOAD_None, NULL);
+
 	TArray<FName> RowNames;
 	RowNames = Tables[0]->GetRowNames();
 	TArray<FString> EachBibles;
 
-	//for (auto& Name : RowNames)
-	//{
-	//	FAfrikaansTable* Info = Tables[0]->FindRow<FAfrikaansTable>(Name, FString(""));
-	//	if (Info->BookNumber == 1 && Info->Chapter == 1)
-	//	{
-	//		EachBibles.Add(Info->Text);
+	for (auto& Name : RowNames)
+	{
+		FAfrikaansTable* Info = Tables[0]->FindRow<FAfrikaansTable>(Name, FString(""));
+		if (Info->BookNumber == 1 && Info->Chapter == 1)
+		{
+			EachBibles.Add(Info->Text);
 
-	//		UE_LOG(LogTemp, Warning, TEXT("%s : %s"), *Info->BookName, *Info->Text);
-	//	}
-	//}
+			UE_LOG(LogTemp, Warning, TEXT("%s : %s"), *Info->BookName, *Info->Text);
+		}
+	}
 }
